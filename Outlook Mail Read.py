e596010cd51018063aa8fc845a775e2e1ae3304f
Abhishek Gurung <34134665+Abhishek-Gurung@users.noreply.github.com>
2023-01-12 11:41:48 +0530

    Update Outlook Mail Read.py

--- Outlook Mail Read.py
+++ Outlook Mail Read.py
@@ -1,12 +1,26 @@
-#import wmi module  
-import wmi  
-   
-# Initializise  the wmi constructor  
-f = wmi.WMI()  
-   
-# Print the header   
-print("Printing the pid   Process name")  
-   
-# all the running processes  
-for process in f.Win32_Process():  
-print(f"{process.ProcessId:<5} {process.Name}")  
+import win32com.client
+ 
+import os
+from datetime import datetime, timedelta
+ 
+outlook = win32com.client.Dispatch('outlook.application')
+mapi = outlook.GetNamespace("MAPI")
+ 
+
+     
+inbox = mapi.GetDefaultFolder(6)
+#inbox = mapi.GetDefaultFolder(6).Folders["IMP"] #Specific folders in outlook
+ 
+messages = inbox.Items
+x=0
+received_dt = datetime.now() - timedelta(days=1)
+received_dt = received_dt.strftime('%m/%d/%Y %H:%M %p')
+messages = messages.Restrict("[ReceivedTime] >= '" + received_dt + "'") #recent mails
+#messages = messages.Restrict("[Subject] = 'Broadcast'")
+messages = messages.Restrict("[SenderEmailAddress] = 'defender-noreply@microsoft.com'")
+for message in messages:
+    print(received_dt,"\n\n-------------------------------------------\n")
+    #print(message.Subject)
+    print(message.Body)
+
+    
